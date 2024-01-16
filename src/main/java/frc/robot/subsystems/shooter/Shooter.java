50aa116cb570e39c341b02d00bf28b7a65084d88
Hatulim <tomer10.katz@gmail.com>
2024-01-16 09:48:40 +0200

    make velocity changable through shuffleboard

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -19,12 +19,14 @@ public class Shooter extends SubsystemBase {
   public CANSparkMax neon;
   public TalonFX motor1;
   public TalonFX motor2;
-
+  
+  double wantedVel = 0;
+  
   /** Creates a new Shooter. */
   public Shooter() {
     neon = new CANSparkMax(NEON_ID, MotorType.kBrushless);
     neon.getEncoder().setPosition(0);
-
+    
     motor1 = new TalonFX(MOTOR_1_ID);
     motor2 = new TalonFX(MOTOR_2_ID);
     motor2.setInverted(true);
@@ -36,26 +38,26 @@ public class Shooter extends SubsystemBase {
     
     SmartDashboard.putData(this);
   }
-
+  
   /**
    * @param vel must be from -1 to 1
    */
   public void neonSetVel(double vel){
     neon.set(vel);
   }
-
+  
   public void neonEncoderSet(double postion){
     neon.getEncoder().setPosition(postion);
   }
-
+  
   public void neonEncoderReset(){
     neonEncoderSet(0);
   }
-
+  
   public double getNeonRev(){
     return neon.getEncoder().getPosition()/NEON_PULES_PER_REV;
   }
-
+  
   public void neonMoveByRev(double vel, double rev){
     double startPos = getNeonRev();
     while (Math.abs(getNeonRev()-startPos+rev)==1){
@@ -63,7 +65,7 @@ public class Shooter extends SubsystemBase {
       System.out.println("another"+(Math.abs(getNeonRev()-startPos+rev))+"rev");
     }
   }
-
+  
   public void falconSetVel(double vel){
     double pow = KS + KV * vel;
     System.out.println("Vel = "+ vel);
@@ -75,7 +77,8 @@ public class Shooter extends SubsystemBase {
     motor1.set(ControlMode.PercentOutput, 0);
     motor2.set(ControlMode.PercentOutput, 0);
   }
-
+  
+  
   @Override
   public void initSendable(SendableBuilder builder) {
       super.initSendable(builder);
@@ -84,7 +87,8 @@ public class Shooter extends SubsystemBase {
       builder.addDoubleProperty("motor 1 speed", ()-> motor1.getSelectedSensorPosition()*10/(FALCON_PULES_PER_REV/360), null);
       builder.addDoubleProperty("motor 2 speed", ()-> motor2.getSelectedSensorPosition()*10/(FALCON_PULES_PER_REV/360), null);
   
-      SmartDashboard.putData("RUN", new InstantCommand(()-> falconSetVel(10000)));
+      SmartDashboard.putData("set wanted vel", new InstantCommand(()-> wantedVel = SmartDashboard.getNumber("wanted vel", 10000)));
+      SmartDashboard.putData("RUN", new InstantCommand(()-> falconSetVel(wantedVel)));
       SmartDashboard.putData("STOP", new InstantCommand(()-> falconStop()));
   }
 
