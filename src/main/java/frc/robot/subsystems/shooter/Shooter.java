4291aa38b784d1761eb1b4da87f9aa5ccbc9f305
Hatulim <tomer10.katz@gmail.com>
2024-01-16 09:29:43 +0200

    add falcon motors to the shooter subsystem

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -5,52 +5,87 @@
 package frc.robot.subsystems.shooter;
 
 import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import static frc.robot.Constants.ShooterConstants.*;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class Shooter extends SubsystemBase {
-  public CANSparkMax neon = new CANSparkMax(NEON_ID, MotorType.kBrushless);
+  public CANSparkMax neon;
+  public TalonFX motor1;
+  public TalonFX motor2;
 
   /** Creates a new Shooter. */
   public Shooter() {
+    neon = new CANSparkMax(NEON_ID, MotorType.kBrushless);
     neon.getEncoder().setPosition(0);
+
+    motor1 = new TalonFX(MOTOR_1_ID);
+    motor2 = new TalonFX(MOTOR_2_ID);
+    motor2.setInverted(true);
+    
+    motor1.config_kP(0, KP);
+    motor1.config_kI(0, KI);
+    motor2.config_kP(0, KP);
+    motor2.config_kI(0, KI);
+    
+    SmartDashboard.putData(this);
   }
 
   /**
    * @param vel must be from -1 to 1
    */
-  public void setVel(double vel){
+  public void neonSetVel(double vel){
     neon.set(vel);
   }
 
-  public void encoderSet(double postion){
+  public void neonEncoderSet(double postion){
     neon.getEncoder().setPosition(postion);
   }
 
-  public void encoderReset(){
-    encoderSet(0);
+  public void neonEncoderReset(){
+    neonEncoderSet(0);
   }
 
-  public double getRev(){
-    return neon.getEncoder().getPosition()/PULES_PER_ROTATION;
+  public double getNeonRev(){
+    return neon.getEncoder().getPosition()/NEON_PULES_PER_REV;
   }
 
-  public void moveByRev(double vel, double rev){
-    double startPos = getRev();
-    while (Math.abs(getRev()-startPos+rev)==1){
-      setVel(vel);
-      System.out.println("another"+(Math.abs(getRev()-startPos+rev))+"rev");
+  public void neonMoveByRev(double vel, double rev){
+    double startPos = getNeonRev();
+    while (Math.abs(getNeonRev()-startPos+rev)==1){
+      neonSetVel(vel);
+      System.out.println("another"+(Math.abs(getNeonRev()-startPos+rev))+"rev");
     }
+  }
+
+  public void falconSetVel(double vel){
+    double pow = KS + KV * vel;
+    System.out.println("Vel = "+ vel);
+    motor1.set(ControlMode.PercentOutput, pow);
+    motor2.set(ControlMode.PercentOutput, pow);
+  }
+
+  public void falconStop(){
+    motor1.set(ControlMode.PercentOutput, 0);
+    motor2.set(ControlMode.PercentOutput, 0);
   }
 
   @Override
   public void initSendable(SendableBuilder builder) {
-      // TODO Auto-generated method stub
       super.initSendable(builder);
-      builder.addDoubleProperty("encoder", this::getRev, null);
+
+      builder.addDoubleProperty("neon encoder", this::getNeonRev, null);
+      builder.addDoubleProperty("motor 1 speed", ()-> motor1.getSelectedSensorPosition()*10/(FALCON_PULES_PER_REV/360), null);
+      builder.addDoubleProperty("motor 2 speed", ()-> motor2.getSelectedSensorPosition()*10/(FALCON_PULES_PER_REV/360), null);
+  
+      SmartDashboard.putData("RUN", new InstantCommand(()-> falconSetVel(10000)));
+      SmartDashboard.putData("STOP", new InstantCommand(()-> falconStop()));
   }
 
   @Override
