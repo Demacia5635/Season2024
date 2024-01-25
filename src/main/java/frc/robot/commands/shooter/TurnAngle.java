ae760b2f62c5a781ada8e9e11fe385547f2b676a
Hatulim <tomer10.katz@gmail.com>
2024-01-25 12:15:46 +0200

    balance the spaces

--- src/main/java/frc/robot/commands/shooter/TurnAngle.java
+++ src/main/java/frc/robot/commands/shooter/TurnAngle.java
@@ -8,41 +8,41 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.shooter.Shooter;
 
 public class TurnAngle extends CommandBase {
-  final Shooter shooter;
-  double turns;
-  double vel;
-  double startPos;
+    final Shooter shooter;
+    double turns;
+    double vel;
+    double startPos;
 
-  /** Creates a new TurnAngle. */
-  public TurnAngle(Shooter shooter, double turns, double vel) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.shooter = shooter;
-    this.turns = turns;
-    this.vel = vel;
-    addRequirements(shooter);
-  }
+    /** Creates a new TurnAngle. */
+    public TurnAngle(Shooter shooter, double turns, double vel) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        this.shooter = shooter;
+        this.turns = turns;
+        this.vel = vel;
+        addRequirements(shooter);
+    }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    startPos = shooter.getAngleEncoder();
-  }
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        startPos = shooter.getAngleEncoder();
+    }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    shooter.setAngleVel(vel);
-  }
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        shooter.setAngleVel(vel);
+    }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    shooter.stopAngle();
-  }
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        shooter.stopAngle();
+    }
 
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return shooter.getAngleEncoder() >= turns+startPos;
-  }
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return shooter.getAngleEncoder() >= turns+startPos;
+    }
 }
