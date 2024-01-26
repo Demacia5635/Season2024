a1645f8cd0b25a763828ca3af2686d33c7c6ef1f
Hatulim <tomer10.katz@gmail.com>
2024-01-27 00:18:44 +0200

    make a falcon version in case switch to a falcon

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -16,7 +16,10 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 public class Shooter extends SubsystemBase {
+
     public CANSparkMax neon;
+    // public TalonFX motorAngle;
+
     public final TalonFX motor1;
     public final TalonFX motor2;
 
@@ -35,6 +38,7 @@ public class Shooter extends SubsystemBase {
 
         neon = new CANSparkMax(NEON_ID, MotorType.kBrushless);
         neon.getEncoder().setPosition(0);
+        // motorAngle = new TalonFX(ANLGE_MOTOR_ID);
 
         SmartDashboard.putData(this);
     }
@@ -45,30 +49,34 @@ public class Shooter extends SubsystemBase {
     public void neonSetPow(double pow){
         neon.set(pow);
     }
+    
+    // public void anlgeSetPow(double pow){
+    //     motorAngle.set(ControlMode.PercentOutput, pow);
+    // }
 
     public void neonStop(){
         neon.stopMotor();
     }
 
+    // public void anlgeStop(){
+    //     motorAngle.set(ControlMode.PercentOutput, 0);
+    // }
+
     public void neonEncoderSet(double postion){
         neon.getEncoder().setPosition(postion);
     }
-
+    
     public void neonEncoderReset(){
         neonEncoderSet(0);
     }
-
-    public double getNEONRev(){
-        return neon.getEncoder().getPosition()/NEON_PULES_PER_REV;
-    }
-  
+    
     public void setVel(double vel){
         double pow = KS + KV * vel;
         System.out.println("power = "+ pow);
         motor1.set(ControlMode.PercentOutput, pow);
         motor2.set(ControlMode.PercentOutput, pow);
     }
-
+    
     public void stop(){
         motor1.set(ControlMode.PercentOutput, 0);
         motor2.set(ControlMode.PercentOutput, 0);
@@ -77,24 +85,31 @@ public class Shooter extends SubsystemBase {
     public void stopAll(){
         neonStop();
         // stop();
+        // anlgeStop();
     }
+    
+    // public double getAnlgeEncoder(){
+    //     return (motorAngle.getSelectedSensorPosition()/PULES_PER_REV) - basePos;
+    // }
+
+    // public void angleResetPos(){
+    //     basePos = motorAngle.getSelectedSensorPosition()/PULES_PER_REV;
+    // }
 
     @Override
     public void initSendable(SendableBuilder builder) {
         super.initSendable(builder);
-
+        
         builder.addDoubleProperty("motor 1 speed", ()-> motor1.getSelectedSensorVelocity()*10/(PULES_PER_REV/360), null);
         builder.addDoubleProperty("motor 2 speed", ()-> motor2.getSelectedSensorVelocity()*10/(PULES_PER_REV/360), null);
         builder.addDoubleProperty("current amper motor 1", ()-> motor1.getSupplyCurrent(), null);
         builder.addDoubleProperty("current amper motor 2", ()-> motor2.getSupplyCurrent(), null);
-        builder.addDoubleProperty("neon encoder", this::getNEONRev, null);
+        builder.addDoubleProperty("neon encoder", ()->neon.getEncoder().getPosition(), null);
+        // builder.addDoubleProperty("angle motor encoder", this::getAnlgeEncoder, null);
         builder.addDoubleProperty("base angle", ()->basePos, null);
     
         SmartDashboard.putData("Reset neon", new InstantCommand(()-> neonEncoderReset()));
+        // SmartDashboard.putData("Reset angle motor", new InstantCommand(()-> angleResetPos()));
     }
 
-    @Override
-    public void periodic() {
-        // This method will be called once per scheduler run
-    }
 }
