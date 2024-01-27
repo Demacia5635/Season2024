270254ea207c2631a4bde126cebe37d17c6d5237
Hatulim <tomer10.katz@gmail.com>
2024-01-27 21:39:40 +0200

    remove neon function

--- src/main/java/frc/robot/commands/shooter/TurnAngle.java
+++ src/main/java/frc/robot/commands/shooter/TurnAngle.java
@@ -4,10 +4,10 @@
 
 package frc.robot.commands.shooter;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shooter.Shooter;
 
-public class TurnAngle extends CommandBase {
+public class TurnAngle extends Command {
     final Shooter shooter;
     double turns;
     double pow;
@@ -25,28 +25,24 @@ public class TurnAngle extends CommandBase {
     // Called when the command is initially scheduled.
     @Override
     public void initialize() {
-        shooter.neonEncoderReset();
-        // shooter.angleResetPos();
+        shooter.angleResetPos();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        shooter.neonSetPow(pow);
-        // shooter.anlgeSetPow(pow);
+        shooter.anlgeSetPow(pow);
     }
 
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
-        shooter.neonSetPow(0);
-        // shooter.anlgeStop();
+        shooter.anlgeStop();
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        // return shooter.getAnlgeEncoder() >= turns+startPos;
-        return shooter.neon.getEncoder().getPosition() >= turns+startPos;
+        return shooter.getAngleEncoder() >= turns+startPos;
     }
 }
