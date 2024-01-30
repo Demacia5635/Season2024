368a9c9d50b084ec992d02f8ffc66ddff3763247
Hatulim <tomer10.katz@gmail.com>
2024-01-30 10:47:34 +0200

    change angle set pow to angel set vel

--- src/main/java/frc/robot/commands/shooter/TurnAngle.java
+++ src/main/java/frc/robot/commands/shooter/TurnAngle.java
@@ -31,7 +31,7 @@ public class TurnAngle extends Command {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        shooter.anlgeSetPow(pow);
+        shooter.anlgeSetVel(pow);
     }
 
     // Called once the command ends or is interrupted.
@@ -43,6 +43,6 @@ public class TurnAngle extends Command {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return shooter.getAngleEncoder() >= turns+startPos;
+        return shooter.getAngleEncoder() >= turns+startPos || shooter.limits();
     }
 }
