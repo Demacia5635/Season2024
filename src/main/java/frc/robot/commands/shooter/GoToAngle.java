a56f29b4d184a599e39dff0d88d82f4ded1562cb
Hatulim <tomer10.katz@gmail.com>
2024-02-01 10:12:18 +0200

    switch to trapezoid

--- src/main/java/frc/robot/commands/shooter/GoToAngle.java
+++ src/main/java/frc/robot/commands/shooter/GoToAngle.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.shooter.Shooter;
 import frc.robot.utils.TrapezoidCalc;
 
-import static frc.robot.Constants.ShooterConstants.*;
+import static frc.robot.Constants.ElevationConstants.*;
 
 public class GoToAngle extends Command {
     
@@ -20,13 +20,14 @@ public class GoToAngle extends Command {
     double acc;
     double endVel;
     double startDis;
-
+    double diffrentDirectionMultyplier = 50;
+    
     /** Creates a new GoToAngle. */
     public GoToAngle(Shooter shooter, double angle, double maxVel, double acc) {
         // Use addRequirements() here to declare subsystem dependencies.
         this.shooter = shooter;
-        this.wantedAngle = angle;
         this.calc = new TrapezoidCalc();
+        this.wantedAngle = angle;
         this.maxVel = maxVel;
         this.acc = acc;
         this.endVel = 0;
@@ -37,8 +38,8 @@ public class GoToAngle extends Command {
     public GoToAngle(Shooter shooter, double angle, double maxVel, double acc, double endVel) {
         // Use addRequirements() here to declare subsystem dependencies.
         this.shooter = shooter;
-        this.wantedAngle = angle;
         this.calc = new TrapezoidCalc();
+        this.wantedAngle = angle;
         this.maxVel = maxVel;
         this.acc = acc;
         this.endVel = endVel;
@@ -47,11 +48,12 @@ public class GoToAngle extends Command {
 
     // Called when the command is initially scheduled.
     /**
-     * @see <a href="https://www.desmos.com/calculator/4ja9zotx82">Desmos Graph</a>
+     * Desmos - {@link https://www.desmos.com/calculator/4ja9zotx82}
      */
     @Override
     public void initialize() {
         shooter.angleBrake();
+        startDis = shooter.getDis();
 
         // wantedDis =  KB * Math.cos(wantedAngle * Math.PI / 180)+Math.sqrt(Math.pow(KA, 2) - Math.pow((KB * Math.sin(wantedAngle * Math.PI / 180)), 2));
         wantedDis = KA * Math.cos(wantedAngle * Math.PI / 180) + Math.sqrt(Math.pow(KA, 2) * Math.pow(Math.cos(wantedAngle * Math.PI / 180), 2) - Math.pow(KA, 2) + Math.pow(KB, 2));
@@ -60,7 +62,13 @@ public class GoToAngle extends Command {
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        shooter.anlgeSetVel(calc.trapezoid(shooter.getAngleVel(), maxVel, endVel, acc, wantedDis - shooter.getDis()));
+        shooter.anlgeSetVel(calc.trapezoid(shooter.getAngleVel(), shooter.getDis() > wantedDis ? maxVel : maxVel*diffrentDirectionMultyplier, endVel,  shooter.getDis() > wantedDis ? acc : acc*diffrentDirectionMultyplier, wantedDis - shooter.getDis()));
+
+        // if (wantedDis > shooter.getDis()){
+        //     shooter.anlgeSetVel(maxVel);
+        // } else {
+        //     shooter.anlgeSetVel(-maxVel);
+        // }
     }
 
     // Called once the command ends or is interrupted.
@@ -69,6 +77,8 @@ public class GoToAngle extends Command {
         shooter.anlgeStop();
     }
 
+    
+    
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
