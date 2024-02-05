bd71f404319886c0498a0c201815fbb7edff1ace
Hatulim <tomer10.katz@gmail.com>
2024-02-05 09:25:13 +0200

    combine ShooterConstants and ElevationConstants for simplysity

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -110,25 +110,13 @@ public final class Constants {
     }
   }
 
-  public static class ShooterConstants {
+    public static class ShooterConstants {
+        public static final int MOTOR_ID = 3;
         public static final int MOTOR_1_ID = 1;
         public static final int MOTOR_2_ID = 2;
-            
-        public static final double KP = 0.3;
-        public static final double KI = 0;
         
-        public static final double KV = 0.25/12000;
-        public static final double KS = 0.15 - KV * 4000;
-        
-        public static final int PULES_PER_REV = 2048;  
-      
         public static final Pose2d BLUE_ALLIANCE_SPEAKER_POSE2D = new Pose2d(-0.15,2.1842, Rotation2d.fromDegrees(0));
         public static final Pose2d RED_ALLIANCE_SPEAKER_POSE2D = new Pose2d(6.5273,2.1842, Rotation2d.fromDegrees(180));
-        
-    }
-    
-    public static class ElevationConstants {
-        public static final int MOTOR_ID = 3;
 
         public static final int PULES_PER_REV = 2048;
         public static final int GEAR_RATIO = 4;
@@ -144,6 +132,5 @@ public final class Constants {
         
         public static final double KP = 0.3;
         public static final double KD = KP / 100;
-
     }
 }
