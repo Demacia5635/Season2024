af9b26b108a59cc92710e9c0f166fa1765911efe
nanonoam <noamper7@gmail.com>
2024-02-13 20:07:55 +0200

    changed autonomous command to path planner

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,5 +1,8 @@
 package frc.robot;
 
+import org.opencv.core.Point;
+
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -8,7 +11,9 @@ import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.PathFollow.Util.pathPoint;
 import frc.robot.commands.chassis.DriveCommand;
+import frc.robot.commands.chassis.PathFollow;
 import frc.robot.commands.vision.GoToNoteCommand;
 import frc.robot.subsystems.chassis.Chassis;
 
@@ -65,7 +70,7 @@ public class RobotContainer implements Sendable{
   public Command getAutonomousCommand() {
     //return null;
     //return new RunCommand(()-> chassis.setModulesAngularVelocity(50), chassis);
-    return new GoToNoteCommand(chassis);
+    // return new GoToNoteCommand(chassis);
     // return new InstantCommand(() -> chassis.resetWheels(), chassis)
     // .andThen(new RunCommand(() -> chassis.setVelocities(new ChassisSpeeds(-2, 0, 0))).withTimeout(2).andThen(new InstantCommand(() -> chassis.stop())));
     //return new RunCommand(() -> chassis.getModule(2).setAngularVelocity(600));
@@ -74,5 +79,18 @@ public class RobotContainer implements Sendable{
     //return new RunCommand(()->{chassis.getModule(2).setAngularPower(0.049 + 300*0.0003);},chassis).withTimeout(3)
     //  .andThen(new InstantCommand(()->{SmartDashboard.putNumber("FF TEST",  chassis.getModule(2).getAngularVelocity());
     //chassis.stop();}));
+    pathPoint[] pointArrUp = {new pathPoint(3, 2.5, Rotation2d.fromDegrees(0), 0, false), new pathPoint(3, 3.25, Rotation2d.fromDegrees(0), 0, false), new pathPoint(3, 4, Rotation2d.fromDegrees(0), 0, false)};
+    pathPoint[] pointArrDown = {new pathPoint(3, 3, Rotation2d.fromDegrees(0), 0, false), new pathPoint(3, 2.5, Rotation2d.fromDegrees(0), 0, false), new pathPoint(3, 1.75, Rotation2d.fromDegrees(0), 0, false), new pathPoint(3, 0.5, Rotation2d.fromDegrees(0), 0, false)};
+
+    pathPoint[] pointArrBack = {new pathPoint(2.5, 2.2, Rotation2d.fromDegrees(0), 0, false), new pathPoint(3, 2.2, Rotation2d.fromDegrees(0), 0, false), new pathPoint(3.5, 2.2, Rotation2d.fromDegrees(0), 0, false), new pathPoint(4, 2.2, Rotation2d.fromDegrees(0), 0, false)};
+    pathPoint[] pointArrForward = {new pathPoint(3.5, 2.2, Rotation2d.fromDegrees(0), 0, false), new pathPoint(3, 2.2, Rotation2d.fromDegrees(0), 0, false), new pathPoint(2.5, 2.2, Rotation2d.fromDegrees(0), 0, false), new pathPoint(2, 2.2, Rotation2d.fromDegrees(0), 0, false)};
+
+    return new PathFollow(chassis,pointArrUp , 1, 1).andThen(new PathFollow(chassis,pointArrDown , 2, 1.5).
+    andThen(new PathFollow(chassis,pointArrUp , 2.5, 1.75).andThen(new PathFollow(chassis,pointArrDown , 3, 2).
+    andThen(new PathFollow(chassis,pointArrUp , 3.5, 2).andThen(new PathFollow(chassis,pointArrDown , 4, 2.5))))));
+
+    // return new PathFollow(chassis,pointArrBack , 1, 1).andThen(new PathFollow(chassis,pointArrForward , 2, 1.5).
+    // andThen(new PathFollow(chassis,pointArrBack , 2.5, 1.75).andThen(new PathFollow(chassis,pointArrForward , 3, 2).
+    // andThen(new PathFollow(chassis,pointArrBack , 3.5, 2).andThen(new PathFollow(chassis,pointArrForward , 4, 2.5))))));
   }
 }
